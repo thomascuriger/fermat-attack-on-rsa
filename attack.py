479bb0925052a3278c9a2694508b8ad74bb71446
thomas.curiger <thomas.curiger@deimos.ch>
2022-07-07 11:54:58 +0200

    add miller rabin test

--- attack.py
+++ attack.py
@@ -1,16 +1,59 @@
 import math
+import random
 
-def generatePrime():
-    print("prime generated")
+# utility functions
 
-def isMillerRabinPrime():
+# (x^y) % p
+def moduloMult(x, y, p):
+    res = 1;
+    x = x % p;
+    while (y > 0):
+        if (y & 1):
+            res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    return res;
+
+def generatePrime(n, k):
+    while (isMillerRabinPrime(n, k) == False):
+        n+=1;
+    return n;
+
+# pre: n number, k no of tries
+def isMillerRabinPrime(n, k):
+    d = n - 1;
+    while (d%2 == 0):
+        d //= 2;
     print("check prime")
+    for i in range(k):
+        if (millerRabinTest(d, n) == False):
+            print("not prime")
+            return False
+    print("is prime")
+    return True
+
+def millerRabinTest(d, n):
+    a = 2 + random.randint(1, n-4)
+    x = moduloMult(a, d, n);
+    if (x == 1 or x == n - 1):
+        return True
+    while (d != n - 1):
+        x = (x * x) % n;
+        d *= 2;
+        if (x == 1):
+            return False;
+        if (x == n - 1):
+            return True;
+    return False;
 
 
 def calculateRSA():
-    p = generatePrime()
-    q = generatePrime()
-    higher = max(p,q)
+    y = 1;
+    # y big number
+    p = generatePrime(y);
+    # q next higher than p
+    q = generatePrime(p + 1);
+    higher = max(p,q); # sowieso q?
     n = p * q
     d = getRsaD(p,q)
     # d next great prime
@@ -59,4 +102,7 @@ def main():
         i += 1
     getStatistic(results)
 
-main()
+#main()
+
+# isMillerRabinPrime(7761, 10)
+# works
